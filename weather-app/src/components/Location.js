33bf41f948b7308f4d340c7e516355b753449909
Juan Pedraza <juanpedraza@localhost.localdomain>
2018-11-27 20:57:11 -0500

    add weather-app project

---
+++ weather-app/src/components/Location.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Location = (props) =>  {
+    console.log(props);
+    debugger;
+
+    return (<div><h1>props</h1></div>);
+};
+
+export default Location;
---
+++ weather-app/src/components/WeatherTemperature.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WeatherTemperature = () =>  (
+    <div>WeatherExtraInfo</div>
+);
+
+export default WeatherTemperature;
