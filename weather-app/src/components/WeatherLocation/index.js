722f7d47f673228eefa7c0e5fa87de437e4107c4
juan.pedraza <juan.pedraza@unionsoluciones.com.co>
2018-12-05 17:39:46 -0500

    add promise to fetch data

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -4,6 +4,11 @@ import WeatherData from './WeatherData';
 import './styles.css';
 import {SUN, WINDY} from './../../constants/weather'
 
+const location = 'Bogota,co';
+const api_key = 'bd7fc4076bb98b8363191a671a273427';
+const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather'
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`
+
 const data = {
     temperature: 5,
     weatherState: SUN,
@@ -30,7 +35,13 @@ class WeatherLocation extends Component {
     }
     
     handleUpdateClick = () => {
-        console.log("actualizado")
+        fetch (api_weather).then(resolve => {
+            return resolve.json();
+        }).then(data => {
+            console.log(data)
+            debugger;
+        });
+        
         this.setState({
             data: data2,
         });
